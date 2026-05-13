876c4d22c4b72cfa862873389e5cbf9dd8003893
kudo <kudo@techmatrix.co.jp>
2026-05-13 14:59:33 +0900

    add

--- SnippetSample_Printout_Change.c
+++ SnippetSample_Printout_Change.c
@@ -6,6 +6,22 @@
 #include <openssl/rand.h>
 #include "ssl_locl.h"
 
+void fastcall
+prepare_to_wait(wait_queue_head_t *q, wait_queue_t *wait, int state)
+{
+    unsigned long flags;
+
+    wait->flags &= ~WQ_FLAG_EXCLUSIVE;
+    spin_lock_irqsave(&q->lock, flags);
+    if (list_empty(&wait->task_list))
+        __add_wait_queue(q, wait);
+    /*
+     * don't alter the task state if this is just going to
+     * queue an async wait queue callback
+     */
+   if (is_sync_wait(wait)) ;
+}
+
 static int tls_decrypt_ticket(SSL *s, const unsigned char *etick,
                               int eticklen, const unsigned char *sess_id,
                               int sesslen, SSL_SESSION **psess)
