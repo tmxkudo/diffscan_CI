be3020125b7cf75f0b6b281de76c91089ab0f19c
kudo <kudo@techmatrix.co.jp>
2026-07-08 13:42:33 +0900

    mod

--- SnippetSample_Printout_Change.c
+++ SnippetSample_Printout_Change.c
@@ -6,21 +6,6 @@
 #include <openssl/rand.h>
 #include "ssl_locl.h"
 
-void fastcall
-prepare_to_wait(wait_queue_head_t *q, wait_queue_t *wait, int state)
-{
-    unsigned long flags;
-
-    wait->flags &= ~WQ_FLAG_EXCLUSIVE;
-    spin_lock_irqsave(&q->lock, flags);
-    if (list_empty(&wait->task_list))
-        __add_wait_queue(q, wait);
-    /*
-     * don't alter the task state if this is just going to
-     * queue an async wait queue callback
-     */
-   if (is_sync_wait(wait)) ;
-}
 
 static int tls_decrypt_ticket(SSL *s, const unsigned char *etick,
                               int eticklen, const unsigned char *sess_id,
@@ -35,16 +20,26 @@ static int tls_decrypt_ticket(SSL *s, const unsigned char *etick,
     EVP_CIPHER_CTX *ctx;
     SSL_CTX *tctx = s->initial_ctx;
 
-        pitem *item;
+    pitem *item;
     hm_fragment *frag;
-    int al;
+    int ret;
 
-    *ok = 0;
-    item = pqueue_peek(s->d1->buffered_messages);
-    if (item == NULL)
-        return 0;
+    do {
+        item = pqueue_peek(s->d1->buffered_messages);
+        if (item == NULL)
+            return 0;
 
-    frag = (hm_fragment *)item->data;
+        frag = (hm_fragment *)item->data;
+
+        if (frag->msg_header.seq < s->d1->handshake_read_seq) {
+            /* This is a stale message that has been buffered so clear it */
+            pqueue_pop(s->d1->buffered_messages);
+            dtls1_hm_fragment_free(frag);
+            pitem_free(item);
+            item = NULL;
+            frag = NULL;
+        }
+    } while (item == NULL);
 
     /* Don't return if reassembly still in progress */
     if (frag->reassembly != NULL)
